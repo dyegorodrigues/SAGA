93897d7f9f454f5b917aa088ff3ed1a60f95e6a3
dyegorodrigues <fentanespcsc@gmail.com>
2026-08-08 19:56:53 -0300

    P22.4: registra N1.09 na Journey

--- src/curriculum/fichas/index.ts
+++ src/curriculum/fichas/index.ts
@@ -10,6 +10,7 @@ import { N1_04 } from './jornada/N1.04';
 import { N1_06 } from './jornada/N1.06';
 import { N1_07 } from './jornada/N1.07';
 import { N1_08 } from './jornada/N1.08';
+import { N1_09 } from './jornada/N1.09';
 import { N1_10 } from './jornada/N1.10';
 import { N1_11 } from './jornada/N1.11';
 import { N1_13 } from './jornada/N1.13';
@@ -35,7 +36,7 @@ import { GM_12 } from './jornada/GM.12';
 import { JARDIM_FICHAS } from './dojo/jardim';
 
 export const JOURNEY_FICHAS = [
-  N1_01, N1_02, N1_03, N1_04, N1_06, N1_07, N1_08, N1_10, N1_11, N1_13,
+  N1_01, N1_02, N1_03, N1_04, N1_06, N1_07, N1_08, N1_09, N1_10, N1_11, N1_13,
   N2_01,
   N3_09, N3_10, N3_11,
   N4_02, N4_03, N4_04, N4_06, N4_07, N4_08, N4_09,
